598ae33512d3269f936edcea6c29e5f2dccb248a
Bytech-Technology <bytechsuppor@gmail.com>
2025-09-05 22:35:13 -0300

    Expose tray control and current song/cover update via preload
    - Added `electronAPI` in preload to handle IPC communication for tray controls
    - Implemented `onTrayControl` to listen for Play/Pause, Next, and Previous actions from system tray
    - Added `updateCurrentSong` to send current song title to main process
    - Added `updateCurrentCover` to send current song cover (or null) to main process for tray icon

--- preload.js
+++ preload.js
@@ -3,3 +3,9 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('api', {
   getMusicList: () => ipcRenderer.invoke('get-music-list')
 });
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  onTrayControl: (callback) => ipcRenderer.on('tray-control', (_, action) => callback(action)),
+  updateCurrentSong: (title) => ipcRenderer.send('update-current-song', title),
+  updateCurrentCover: (cover) => ipcRenderer.send('update-current-cover', cover) // 🔥 nueva función
+});
